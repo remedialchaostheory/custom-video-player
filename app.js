e583696c57122d5c514a0491d1154bd8425c1700
remedialchaostheory <remedialchaostheory@users.noreply.github.com>
2019-08-07 12:53:05 -0700

    Add skip functionality

--- app.js
+++ app.js
@@ -22,11 +22,16 @@
     toggle.textContent = icon;
   }
 
+  function skip() {
+    video.currentTime += parseFloat(this.dataset.skip);
+  }
+
 
   /* Hook up event listeners */
   video.addEventListener('click', togglePlay);
   video.addEventListener('play', updateButton);
   video.addEventListener('pause', updateButton);
   toggle.addEventListener('click', togglePlay);
+  skipButtons.forEach(button => button.addEventListener('click', skip));
 
 })();
